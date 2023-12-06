f4e6e0991b65068afb42fd053750be6f1d1ce010
Leonardo Gabriel <leonardogabriel8@hotmail.com>
2023-12-06 15:20:48 -0300

    ajustando

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   url: {
-    //service: '${URL_API_MG}'
-    service: 'https://meninosdoguarasapi-production.up.railway.app'
+    service: '${URL_API_MG}'
+    //service: 'https://meninosdoguarasapi-production.up.railway.app'
   }
 }
